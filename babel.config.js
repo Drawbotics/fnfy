a0f5b2e4b87f225a1cf812cf1485aafc88edb1de
Lorenzo Ruiz <lars.bs@hotmail.com>
2019-04-12 16:53:13 +0200

    Compile the lib

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      modules: false,
-      useBuiltIns: 'entry',
-      forceAllTransforms: true,
-    }],
+    ['@babel/preset-env', { modules: false }],
   ],
   plugins: [
     '@babel/plugin-proposal-export-default-from',
